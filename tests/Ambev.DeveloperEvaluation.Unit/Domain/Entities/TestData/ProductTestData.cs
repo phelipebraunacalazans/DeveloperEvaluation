657025ca4045a2051f301f84bb718724b209c1e6
Phelipe Brauna Calazans <phelipebrauna@gmail.com>
2025-07-31 08:52:00 -0300

    implement tests product

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/TestData/ProductTestData.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
+
+public static class ProductTestData
+{
+    private static readonly Faker<Product> ProductFaker = new Faker<Product>()
+        .RuleFor(c => c.Id, f => f.Random.Guid())
+        .RuleFor(u => u.Name, f => f.Lorem.Sentence(4))
+        .RuleFor(u => u.Price, f => f.Random.Decimal(min: 1, max: 2000.00M));
+
+    public static Product GenerateValid()
+    {
+        return ProductFaker.Generate();
+    }
+}
